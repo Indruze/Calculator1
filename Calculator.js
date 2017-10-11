9627ccac5e491da8d4969dec46029ac179c2c940
Indrė <krasauskaite.i@gmail.com>
2017-10-11 17:54:03 +0300

    Initial commit

---
+++ Calculator.js
@@ -0,0 +1,31 @@
+var result = 0;
+var action = 0;
+
+
+window.onload = function() {
+    //kai naršyklės langas užsikrauna arba persikrauna
+    //atliekamas veiksmas
+    //alert('Valio'); dialogo langas
+    document.getElementById("result").innerHTML = result;
+};
+ function clear(){
+     result = 0;
+     document.getElementById("result").innerHTML = result;
+ }
+
+ function zero(){
+     var zero = 0;
+     action =  zero;
+     documnet.getElementById("result").innerHTML = zero;
+ }
+
+ function one(){
+     var one = 1;
+     documnet.getElementById("result").innerHTML = one;
+ }
+
+
+
+/**
+ * Created by Indrė on 2017-10-11.
+ */
