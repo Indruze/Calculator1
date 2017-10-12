8d84fa75672ab9fb56a1ec0ce2161bb88cc096d1
Indrė <krasauskaite.i@gmail.com>
2017-10-12 13:09:25 +0300

    Something

--- Calculator.js
+++ Calculator.js
@@ -1,5 +1,7 @@
 var result = '0';
 var action = '';
+var number = '';
+var operation = 'plus';
 
 
 window.onload = function() {
@@ -72,6 +74,81 @@ function nine(){
          action = action + '' + number;
          document.getElementById("result").innerHTML = action;
      }
+ }
+
+ function plusNumbers(){
+    result = action;
+    action = '';
+    dots = 0;
+    operation = 'plus';
+    document.getElementById("result").innerHTML = '+';
+ }
+
+function minusNumbers(){
+    result = action;
+    action = '';
+    dots = 0;
+    operation = 'minus';
+    document.getElementById('result').innerHtml = '-';
+}
+
+function timesNumbers(){
+    result = action;
+    action = '';
+    dots = 0;
+    operation = 'times';
+    document.getElementById('result').innerHtml = '*';
+}
+
+function divideNumbers(){
+    result = action;
+    action = '';
+    dots = 0;
+    operation = 'divide';
+    document.getElementById('result').innerHtml = '/';
+}
+
+function backSpace(){
+    var numberLength = action.length - 1;
+    if (numberLength > -1){
+        action = action.substring(0, numberLength);
+    }
+    if(action == ''){
+        action = 0;
+    }
+    document.getElementById('result').innerHTML = action;
+}
+
+var dots = 0;
+
+function addDot(){
+    if(dots == 0){
+        dots += 1;
+        action=action + ".";
+        document.getElementById('result').innerHTML = action;
+    }
+}
+
+ function showResult() {
+     result = parseFloat(result);
+     if (operation == 'plus') {
+         result += parseFloat(action) //+ parseFloat(action); //parseFloat tekstini formata pakeicia i realiuosius skaicius.
+     }
+     else if(operation == 'minus'){
+         result -= parseFloat(action);
+     }
+     else if(operation == 'times') {
+         result *= parseFloat(action)
+     }
+     else if(operation == 'divide') {
+         result /= parseFloat(action);
+     }
+     else {
+         result = '0';
+     }
+     document.getElementById('result').innerHTML = result;
+
+     result = action = operation = ''; //Išvalome visus duomenis skaičiuotuve
  }
 
 
